6e15500b9031facec2a78a184be9e741956699f7
2peng23 <joelrioflorido@gmail,com>
2024-04-08 13:36:57 +0800

    michaelthedog

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'usertype' => '1',
+            'municipality' => '',
+        ]);
     }
 }
